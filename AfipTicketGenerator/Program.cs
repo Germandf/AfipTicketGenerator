dd6e14c794aee2157a2649967086430ba9a1aa70
German De Francesco <germandefrancesco@hotmail.com>
2021-12-08 14:13:13 -0300

    generate all days until now since day's ago's input

--- AfipTicketGenerator/Program.cs
+++ AfipTicketGenerator/Program.cs
@@ -38,43 +38,42 @@ await page.ClickAsync("input[name='F1:btnSiguiente']");
 await page.FillAsync("input[name='F1:password']", password);
 await page.ClickAsync("input[name='F1:btnIngresar']");
 
-// Generate Ticket
-var newPage = await context.RunAndWaitForPageAsync(async () =>
+// Generate tickets
+for(int day = days; day >= 0; day--)
 {
-    await page.ClickAsync("text=Comprobantes en línea");
-});
-await newPage.WaitForLoadStateAsync();
-await newPage.ClickAsync("input[value='DE FRANCESCO LUIS']");
-await newPage.WaitForLoadStateAsync(LoadState.NetworkIdle);
-await newPage.GotoAsync("https://serviciosjava2.afip.gob.ar/rcel/jsp/buscarPtosVtas.do");
-await newPage.SelectOptionAsync("select[id='puntodeventa']", "2");
-await newPage.SelectOptionAsync("select[id='universocomprobante']", "2");
-await newPage.ClickAsync("input[value='Continuar >']");
-await newPage.FillAsync("input[name='fechaEmisionComprobante']", DateTime.Today.AddDays(-days).ToString("dd/MM/yyyy"));
-await newPage.SelectOptionAsync("select[id='idconcepto']", "1");
-await newPage.ClickAsync("input[value='Continuar >']");
-await newPage.SelectOptionAsync("select[id='idivareceptor']", "5");
-await newPage.SetCheckedAsync("input[id='formadepago1']", true);
-await newPage.ClickAsync("input[value='Continuar >']");
-for (int i = 0; i < products.Count - 1; i++)
-{
-    await newPage.ClickAsync("input[value='Agregar línea descripción']");
+    var newPage = await context.RunAndWaitForPageAsync(async () =>
+    {
+        await page.ClickAsync("text=Comprobantes en línea");
+    });
+    await newPage.WaitForLoadStateAsync();
+    await newPage.ClickAsync("input[value='DE FRANCESCO LUIS']");
+    await newPage.WaitForLoadStateAsync(LoadState.NetworkIdle);
+    await newPage.GotoAsync("https://serviciosjava2.afip.gob.ar/rcel/jsp/buscarPtosVtas.do");
+    await newPage.SelectOptionAsync("select[id='puntodeventa']", "2");
+    await newPage.SelectOptionAsync("select[id='universocomprobante']", "2");
+    await newPage.ClickAsync("input[value='Continuar >']");
+    await newPage.FillAsync("input[name='fechaEmisionComprobante']", DateTime.Today.AddDays(-day).ToString("dd/MM/yyyy"));
+    await newPage.SelectOptionAsync("select[id='idconcepto']", "1");
+    await newPage.ClickAsync("input[value='Continuar >']");
+    await newPage.SelectOptionAsync("select[id='idivareceptor']", "5");
+    await newPage.SetCheckedAsync("input[id='formadepago1']", true);
+    await newPage.ClickAsync("input[value='Continuar >']");
+    for (int i = 0; i < products.Count; i++)
+    {
+        await newPage.FillAsync($"textarea[id='detalle_descripcion{i + 1}']", products[i].Name);
+        await newPage.FillAsync($"input[id='detalle_cantidad{i + 1}']", products[i].Quantity);
+        await newPage.SelectOptionAsync($"select[id='detalle_medida{i + 1}']", "1");
+        await newPage.FillAsync($"input[id='detalle_precio{i + 1}']", products[i].Quantity);
+        if (i < products.Count - 1)
+            await newPage.ClickAsync("input[value='Agregar línea descripción']");
+    }
+    await newPage.ClickAsync("input[value='Continuar >']");
+    newPage.Dialog += (_, dialog) => dialog.AcceptAsync();
+    await newPage.ClickAsync("input[id='btngenerar']");
+    /* TODO DOWNLOAD AND PRINT
+    var download = await page.RunAndWaitForDownloadAsync(async () =>
+    {
+        await newPage.ClickAsync("input[value='Imprimir...']");
+    });
+    */
 }
-for (int i = 0; i < products.Count; i++)
-{
-    await newPage.FillAsync($"textarea[id='detalle_descripcion{i+1}']", products[i].Name);
-    await newPage.FillAsync($"input[id='detalle_cantidad{i+1}']", products[i].Quantity);
-    await newPage.SelectOptionAsync($"select[id='detalle_medida{i+1}']", "1");
-    await newPage.FillAsync($"input[id='detalle_precio{i+1}']", products[i].Quantity);
-}
-await newPage.ClickAsync("input[value='Continuar >']");
-
-newPage.Dialog += (_, dialog) => dialog.AcceptAsync();
-await newPage.ClickAsync("input[id='btngenerar']");
-
-/* TODO PRINT
-var download = await page.RunAndWaitForDownloadAsync(async () =>
-{
-    await newPage.ClickAsync("input[value='Imprimir...']");
-});
-*/
